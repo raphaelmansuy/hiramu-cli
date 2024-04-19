5816c4f4dcac71df9b2a028053c29faba2451a35
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-04-19 10:25:33 +0800

    Update Cargo.lock and Cargo.toml to include the thiserror dependency Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io::Read;
-
+use std::io::{self, Read};
 use clap::{arg, Command};
+use tokio::main;
+
 
 use hiramu::bedrock::ModelName;
 use hiramu_cli::{
@@ -9,6 +10,7 @@ use hiramu_cli::{
     model::Generate,
     model_alias::{model_alias_from_str, ModelAlias},
     provider::Provider,
+    error::GenerationError,
 };
 
 const CARGO_TOML: &str = include_str!("../Cargo.toml");
@@ -26,47 +28,18 @@ fn cli() -> Command {
         .subcommand(
             Command::new("prompt")
                 .about("Ask a question to a LLM")
-                .arg(arg!(<PROMPT> "The prompt to ask. Can contain {input} to read from stdin. For example, 'What is the capital of {input}?' will read a country name from stdin and ask the model for its capital."))                .arg(
-                    arg!(-r --region <REGION> "The region to use")
-                        .required(false)
-                        .default_value("us-west-2"),
-                )
-                .arg(
-                    arg!(-p --profile <PROFILE> "The profile to use")
-                        .required(false)
-                        .default_value("bedrock"),
-                )
-                .arg(
-                    arg!(-m --maxtoken <MAXTOKEN> "The maximum number of tokens to generate")
-                        .required(false)
-                        .default_value("100")
-                        .value_parser(clap::value_parser!(u32)),
-                )
-                .arg(
-                    arg!(-t --temperature <TEMPERATURE> "The temperature to use for generation")
-                        .required(false)
-                        .default_value("0.7")
-                        .value_parser(clap::value_parser!(f32)),
-                )
-                .arg(
-                    arg!(-M --model <MODEL> "The model alias to use for generation")
-                        .required(false)
-                        .default_value("haiku")
-                )
-                .arg(
-                    arg!(-P --provider <PROVIDER> "The provider alias to use for generation ollama or bedrock")
-                        .required(false)
-                        .default_value("bedrock")
-                        .value_parser(clap::value_parser!(Provider)),
-                )
-                .arg(
-                    arg!(-E --endpoint <ENDPOINT> "The provider endpoint to use for generation")
-                        .required(false)
-                        .default_value("http://localhost:11434")),                
+                .arg(arg!(<PROMPT> "The prompt to ask. Can contain {input} to read from stdin."))
+                .arg(arg!(-r --region <REGION> "The region to use").default_value("us-west-2"))
+                .arg(arg!(-p --profile <PROFILE> "The profile to use").default_value("bedrock"))
+                .arg(arg!(-m --maxtoken <MAXTOKEN> "The maximum number of tokens to generate").default_value("100").value_parser(clap::value_parser!(u32)))
+                .arg(arg!(-t --temperature <TEMPERATURE> "The temperature to use for generation").default_value("0.7").value_parser(clap::value_parser!(f32)))
+                .arg(arg!(-M --model <MODEL> "The model alias to use for generation").default_value("haiku"))
+                .arg(arg!(-P --provider <PROVIDER> "The provider alias to use for generation ollama or bedrock").default_value("bedrock").value_parser(clap::value_parser!(Provider)))
+                .arg(arg!(-E --endpoint <ENDPOINT> "The provider endpoint to use for generation").default_value("http://localhost:11434"))
         )
 }
 
-pub async fn generate(
+async fn generate(
     question: &str,
     region: Option<String>,
     profile: Option<String>,
@@ -75,82 +48,64 @@ pub async fn generate(
     model: String,
     provider: Provider,
     endpoint: Option<String>,
-) {
+) -> Result<(), GenerationError> {
     let mut prompt = question.to_string();
 
     if prompt.contains("{input}") {
         let mut input = String::new();
-        std::io::stdin().read_to_string(&mut input).unwrap();
+        io::stdin().read_to_string(&mut input)?;
         prompt = prompt.replace("{input}", input.trim());
     }
 
     match provider {
         Provider::Bedrock => {
-            let model = model_alias_from_str(&model);
-            bedrock_generate(model, region, profile, max_token, temperature, prompt).await;
+            let model = model_alias_from_str(&model).ok_or(GenerationError::ModelNotFoundError)?;
+            bedrock_generate(model, region, profile, max_token, temperature, prompt).await?;
         }
         Provider::Ollama => {
-            let endpoint = endpoint.unwrap_or("http://localhost:11434".to_string());
+            let endpoint = endpoint.unwrap_or_else(|| "http://localhost:11434".to_string());
             let ollama_provider = OllamaProvider::new(endpoint, model);
             ollama_provider.generate(&prompt).await;
         }
     }
+
+    Ok(())
 }
 
 async fn bedrock_generate(
-    model: Option<ModelAlias>,
+    model: ModelAlias,
     region: Option<String>,
     profile: Option<String>,
     max_token: Option<u32>,
     temperature: Option<f32>,
     prompt: String,
-) {
-    // get model_name form model
+) -> Result<(), GenerationError> {
+    let model_name = match model {
+        ModelAlias::Haiku => ModelName::AnthropicClaudeHaiku1x,
+        ModelAlias::Sonnet => ModelName::AnthropicClaudeSonnet1x,
+        ModelAlias::Opus => ModelName::AnthropicClaudeOpus1x,
+        ModelAlias::Mistral7b => ModelName::MistralMistral7BInstruct0x,
+        ModelAlias::Mistral8x7b => ModelName::MistralMixtral8X7BInstruct0x,
+        ModelAlias::MistralLarge => ModelName::MistralLarge,
+    };
+
     match model {
-        Some(ModelAlias::Haiku) => {
-            let model_name = ModelName::AnthropicClaudeHaiku1x;
-            let claude_generator =
-                ClaudeGenerator::new(region, profile, max_token, temperature, Some(model_name));
-            claude_generator.generate(&prompt).await;
+        ModelAlias::Haiku | ModelAlias::Sonnet | ModelAlias::Opus => {
+            let generator = ClaudeGenerator::new(region, profile, max_token, temperature, Some(model_name));
+            generator.generate(&prompt).await;
+
         }
-        Some(ModelAlias::Sonnet) => {
-            let model_name = ModelName::AnthropicClaudeSonnet1x;
-            let claude_generator =
-                ClaudeGenerator::new(region, profile, max_token, temperature, Some(model_name));
-            claude_generator.generate(&prompt).await;
+        ModelAlias::Mistral7b | ModelAlias::Mistral8x7b | ModelAlias::MistralLarge => {
+            let generator = MistralGenerator::new(region, profile, max_token, temperature, Some(model_name));
+            generator.generate(&prompt).await;
         }
-        Some(ModelAlias::Opus) => {
-            let model_name = ModelName::AnthropicClaudeOpus1x;
-            let claude_generator =
-                ClaudeGenerator::new(region, profile, max_token, temperature, Some(model_name));
-            claude_generator.generate(&prompt).await;
-        }
-        Some(ModelAlias::Mistral7b) => {
-            let model_name = ModelName::MistralMistral7BInstruct0x;
-            let mistral_generator =
-                MistralGenerator::new(region, profile, max_token, temperature, Some(model_name));
-            mistral_generator.generate(&prompt).await;
-        }
-        Some(ModelAlias::Mistral8x7b) => {
-            let model_name = ModelName::MistralMixtral8X7BInstruct0x;
-            let mistral_generator =
-                MistralGenerator::new(region, profile, max_token, temperature, Some(model_name));
-            mistral_generator.generate(&prompt).await;
-        }
-        Some(ModelAlias::MistralLarge) => {
-            let model_name = ModelName::MistralLarge;
-            let mistral_generator =
-                MistralGenerator::new(region, profile, max_token, temperature, Some(model_name));
-            mistral_generator.generate(&prompt).await;
-        }
-        _ => {
-            println!("Model not found");
-        }
-    }
+    };
+
+    Ok(())
 }
 
-#[tokio::main]
-async fn main() {
+#[main]
+async fn main() -> Result<(), GenerationError> {
     let matches = cli().get_matches();
 
     match matches.subcommand() {
@@ -162,10 +117,8 @@ async fn main() {
             let temperature = sub_matches.get_one::<f32>("temperature").cloned();
             let model = sub_matches.get_one::<String>("model").cloned().unwrap();
             let endpoint = sub_matches.get_one::<String>("endpoint").cloned();
-            let provider = sub_matches
-                .get_one::<Provider>("provider")
-                .cloned()
-                .unwrap();
+            let provider = sub_matches.get_one::<Provider>("provider").cloned().unwrap();
+
             generate(
                 prompt,
                 region,
@@ -176,12 +129,10 @@ async fn main() {
                 provider,
                 endpoint,
             )
-            .await;
+            .await?;
         }
         Some(("version", _)) => {
-            let cargo_toml: toml::Value =
-                toml::from_str(CARGO_TOML).expect("Failed to parse Cargo.toml");
-
+            let cargo_toml: toml::Value = toml::from_str(CARGO_TOML)?;
             let version = cargo_toml
                 .get("package")
                 .and_then(|package| package.get("version"))
@@ -190,7 +141,8 @@ async fn main() {
 
             println!("hiramu-cli version {}", version);
         }
-        // Help and version commands are handled automatically by clap
         _ => {}
     }
+
+    Ok(())
 }
